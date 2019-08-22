88dfe32bcda5948e8ab34d9f8c72bf30ebcbddca
aheart500 <aheart500@yahoo.com>
2019-08-22 04:51:43 +0200

    Updates

---
+++ precache-manifest.4eb87a39535aa1e5cae27ac31720e207.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "343bb5f849e2af21f7d81b3a1969516c",
+    "url": "/production1/index.html"
+  },
+  {
+    "revision": "c1580ee0d35df709e323",
+    "url": "/production1/static/css/main.6edd8023.chunk.css"
+  },
+  {
+    "revision": "ec740296d78ecf914446",
+    "url": "/production1/static/js/2.13003964.chunk.js"
+  },
+  {
+    "revision": "c1580ee0d35df709e323",
+    "url": "/production1/static/js/main.d5c6ff56.chunk.js"
+  },
+  {
+    "revision": "0a76dbb29eee06f4da3e",
+    "url": "/production1/static/js/runtime~main.ebeb0a60.js"
+  }
+]);
